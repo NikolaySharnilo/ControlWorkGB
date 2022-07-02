a7927181c69e3af6a97bc805f5d832505ce23053
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-07-02 11:43:07 +0500

    Add Program Sempling items from array by lenght <=3

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Task 0: Написать программу, которая из имующегося массива строк формируюет массив из строк, 
+//длина которых меньше или равна 3 символа.
+
+string[] array = { "hello", "2", "world", ":-)", "world" };
+
+string[] SemplingItemArrayByLenght(string[] arr, int lenght)
+{
+    string position = string.Empty;
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i].Length <= 3) count++;
+
+    string[] result = new string[count];
+
+    int a = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result[a] = arr[i];
+            a++;
+        }
+    }
+    return result;
+}
+void PrintArray(string[] arr, string txt)
+{
+    Console.Write($"{txt}");
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    
+    Console.WriteLine();
+}
+string[] arrOut = SemplingItemArrayByLenght(array, 3);
+PrintArray(arrOut, "Результат выборки: ");
+
